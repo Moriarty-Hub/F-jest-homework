fe4f1edce187beef7af05d0eb460985fc34b7481
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-09-14 22:04:24 +0800

    [xdong] finish TODO from 19 to 20

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,27 @@
 import { register } from "../user";
+import axios from "axios";
+import { verifyUsername } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    const username = "xdong";
+    const password = "123456";
+    const expected = { username, password };
+    const request = { data: expected };
+    axios.post.mockResolvedValue(request);
+    await expect(register(username, password)).resolves.toEqual(expected);
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    const username = "xdong";
+    const password = "123456";
+    const actual = new Error("wrong username or password");
+    verifyUsername.mockImplementation(() => false);
+    await expect(register(username, password)).rejects.toThrowError(actual);
   });
 });
