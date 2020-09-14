44d58f42914bc9d8d58f4934273ef88c90046b8f
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-09-14 20:44:07 +0800

    [xdong] finish TODO 13

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,14 @@
+import axios from "axios";
+import getUsers from "../users";
+
+jest.mock("axios");
+
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
+  test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
+    const expected = { username: "xdong" };
+    const actual = { data: expected };
+    axios.get.mockResolvedValue(actual);
+    await expect(getUsers()).resolves.toEqual(expected);
   });
 });
